59b9cd3d1d3c4f2e720a31f35009a43f8025d5e4
E41231473 <e41231473@student.polije.ac.id>
2025-05-29 04:07:15 +0700

    __init__,self,variable

---
+++ hello.py
@@ -0,0 +1,39 @@
+print("Hello, World!")
+
+if 5 > 6:
+    print("Hello, World!")
+else:
+    print('5')
+class variable:
+
+    a = 10
+    b = 20
+    c = "benar"
+    d = "salah"
+
+    if a < b:
+        print(c)
+    else:
+        print(d)
+
+    if c == d:
+        print("c sama dengan d")
+    else:
+        print("d sama dengan c")
+
+class mahasiswa:
+    def __init__(self,nama,umur,nilai):
+        self.nama = nama
+        self.umur = umur
+        self.nilai = nilai
+
+    def cek_nilai(self):
+        if self.nilai < 80:
+            print("remidi")
+        else:
+            print("lulus")
+
+m1 = mahasiswa("willy",21,79)
+m1.cek_nilai()
+
+
