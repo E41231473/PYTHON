89f810a12673185e346d66206ff546ea00b7c543
E41231473 <e41231473@student.polije.ac.id>
2025-05-29 19:18:42 +0700

    variable dan tipe data

---
+++ Variables&Types.py
@@ -0,0 +1,26 @@
+#integr
+myint = 7   
+print(myint)
+a, b = 7 ,8
+print(a,b)
+
+#float
+myfloat = 7.0 
+print(myfloat)
+myfloat = float(7) 
+print(myfloat)
+
+#string
+mystring1 = "hello "
+mystring2 = 'hello '
+mystring3 = "hello it's me"
+print(mystring1 + 
+      mystring2 + 
+      mystring3)
+
+satu = 1
+dua = 2
+sapa = 'hello'
+print(satu, dua, sapa)
+print(satu + dua, sapa)
+print(satu, str(dua) + sapa)
